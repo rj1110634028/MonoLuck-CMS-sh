2ab8e2646cbb11d7b799e4c5ba757d8d7bc74dbc
Jonathan <jo901025@gmail.com>
2022-05-16 12:13:24 +0800

    first try

--- start.sh
+++ start.sh
@@ -6,21 +6,19 @@ sudo apt-get install -y git
 echo -e "Install mysql"
 sudo apt-get install -y mysql-server
 sudo mysql --skip-column-names -B -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'toor';"
-sudo mysql --skip-column-names -B -e "CREATE DATABASE 'locker_cms';"
+sudo mysql -B -e "CREATE DATABASE 'locker_cms';"
 
 echo -e "Install php"
 sudo apt-get install -y php libapache2-mod-php php-mysql
 
 echo -e "Install composer"
 cd /tmp
-curl -sS https://getcomposer.org/installer | php
+sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 echo -e "Install nodejs"
- apt-get install nodejs -y
-
-echo choco install nvm
-choco install nvm -y
+sudo apt-get install nodejs -y
+sudo npm install -g npm@latest
 
 echo -e "clone Project"
 echo -e "Laravel"
@@ -28,8 +26,9 @@ cd /home
 git clone https://github.com/rj1110634028/MonoLuck-CMS-Back.git
 cd ./MonoLuck-CMS-Back
 composer install
+
 echo -e "React"
 cd /home
 git clone https://github.com/leonnn124/luck-cms.git
-cd ./MonoLuck-CMS-Back
-composer install
+cd ./luck-cms
+npm install
