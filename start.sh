cab99c252a392271bc40ea0cde52715642a4b4d4
Jonathan <jo901025@gmail.com>
2022-05-16 12:26:30 +0800

    rm sudo

--- start.sh
+++ start.sh
@@ -1,24 +1,24 @@
-sudo apt-get update
+apt-get update
 
 echo -e "Install git"
-sudo apt-get install -y git
+apt-get install -y git
 
 echo -e "Install mysql"
-sudo apt-get install -y mysql-server
-sudo mysql --skip-column-names -B -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'toor';"
-sudo mysql -B -e "CREATE DATABASE 'locker_cms';"
+apt-get install -y mysql-server
+mysql --skip-column-names -B -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'toor';"
+mysql -B -e "CREATE DATABASE 'locker_cms';"
 
 echo -e "Install php"
-sudo apt-get install -y php libapache2-mod-php php-mysql
+apt-get install -y php libapache2-mod-php php-mysql
 
 echo -e "Install composer"
 cd /tmp
-sudo curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
 
 echo -e "Install nodejs"
-sudo apt-get install nodejs -y
-sudo npm install -g npm@latest
+apt-get install nodejs -y
+npm install -g npm@latest
 
 echo -e "clone Project"
 echo -e "Laravel"
